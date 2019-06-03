a1247910f0f94e003bbd67e2edb153c9a360530c
jepidoptera <jepidoptera@live.com>
2019-06-02 23:33:18 -0500

    changed default route

--- server.js
+++ server.js
@@ -45,39 +45,41 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 // Routes
 
 // A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
+app.get("/", function(req, res) {
     // First, we grab the body of the html with axios
     axios.get("http://www.echojs.com/").then(function(response) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
         console.log("got some data");
+        let results = [];
         // Now, we grab every h2 within an article tag, and do the following:
-        $("article h2").each(function(i, element) {
-        // Save an empty result object
-        var result = {};
+        $("div.news-card-body").each(function(i, element) {
+            // Save an empty result object
+            var result = {};
 
-        // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
-            .children("a")
-            .text();
-        result.link = $(this)
-            .children("a")
-            .attr("href");
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
 
-        // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-            .then(function(dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-            })
-            .catch(function(err) {
-            // If an error occurred, log it
-            console.log(err);
-            });
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function(dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    // If an error occurred, log it
+                    console.log(err);
+                });
+            results.push(result);
         });
 
         // Send a message to the client
-        res.send("Scrape Complete");
+        res.render("Scrape Complete");
     });
 });
 
